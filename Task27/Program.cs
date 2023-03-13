a0d1c07bd26cf91cc7a365f49b23529f9bca9187
Victoria Poladiy <vikapoladiy@gmail.com>
2023-03-13 19:56:52 +0300

    Решена 27-я задача

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -2,3 +2,30 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int SumDigit(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        int lastDigit = num % 10;
+        sum = sum + lastDigit;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    int sumDigit = SumDigit(number);
+    Console.WriteLine($"Сумма цифр числа {number} = {sumDigit}");
+}
+else
+{
+    number = number * (-1);
+    int sumDigit = SumDigit(number);
+    Console.WriteLine($"Сумма цифр числа -{number} = {sumDigit}");
+}
+
